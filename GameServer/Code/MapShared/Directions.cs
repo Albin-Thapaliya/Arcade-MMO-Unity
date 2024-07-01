bf7e56d1c993d4ee886db27785b46551da506a79
Albin Thapaliya <wingsbroken04@gmail.com>
2024-07-01 22:02:20 +0200

    Setting up Map & Position ( Server )

---
+++ GameServer/Code/MapShared/Directions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapHandler
+{
+    public enum Direction
+    {
+        SOUTH = 2,
+        NORTH = 8,
+        LEFT = 4,
+        RIGHT = 6,
+        NONE = 5
+    }
+
+    public static class DirectionExtensions
+    {
+        public static Direction Rotate90(this Direction dir)
+        {
+            switch(dir)
+            {
+                case Direction.RIGHT:
+                    return Direction.NORTH;
+                case Direction.NORTH:
+                    return Direction.LEFT;
+                case Direction.LEFT:
+                    return Direction.SOUTH;
+                case Direction.SOUTH:
+                    return Direction.RIGHT;
+            }
+            return Direction.NONE;
+        }
+    }
+}
---
+++ GameServer/Tests/TestPacketSerialization.cs
@@ -0,0 +1,31 @@
+﻿using Common.Networking;
+using Common.Networking.Packets;
+using NUnit.Framework;
+
+namespace Common.Tests
+{
+    [TestFixture]
+    class TestPacketSerialization
+    {
+        [Test]
+        public void TestLoginPacketSerialization()
+        {
+            LoginPacket loginPacket = new LoginPacket()
+            {
+                Login = "Wololo",
+                Password = "Walala"
+            };
+
+            byte[] serialized = PacketSerializer.Serialize(loginPacket);
+
+            object deserialized = PacketSerializer.Deserialize(serialized);
+
+            Assert.That(deserialized is LoginPacket);
+
+            LoginPacket deserializedLoginPacket = (LoginPacket)deserialized;
+
+            Assert.AreEqual(deserializedLoginPacket.Login, loginPacket.Login);
+            Assert.AreEqual(deserializedLoginPacket.Password, loginPacket.Password);
+        }
+    }
+}
